2d2a1e5fb710abc19c4e220cbb2f5358fc5b0b69
Piyush Kumar Tiwari <piyusht823@gmail.com>
2023-10-06 18:03:21 +0530

    uploaded front end

---
+++ src/components/controller.vue
@@ -0,0 +1,72 @@
+<!-- <template>
+    <div>
+      <h2>Vue.js App</h2>
+      <button @click="fetchDataFromSpringBoot">Fetch Data</button>
+      <p>Data from Spring Boot: {{ responseData }}</p>
+    </div>
+  </template>
+  
+  <script>
+  import axios from 'axios';
+  
+  export default {
+    data() {
+      return {
+        responseData: ''
+      };
+    },
+    methods: {
+      fetchDataFromSpringBoot() {
+        axios.get('http://localhost:8080/empdata/getAll')
+          .then(response => {
+            this.responseData = response.data;
+          })
+          .catch(error => {
+            console.error(error);
+          });
+      }
+    }
+  };
+  </script>
+  
+  <style>
+  /* Add your CSS styles here */
+  </style>
+   -->
+
+
+
+
+   <template>
+    <div>
+      <h2>Vue.js App</h2>
+      <button @click="fetchEmployee">Fetch Data</button>
+      <p>Data from Spring Boot: {{ employee }}</p>
+    </div>
+  </template>
+  
+<script setup>
+import {ref, onMounted} from 'vue'
+import axios from 'axios';
+
+const employee = ref([])
+
+    function  fetchEmployee() {
+        axios.get('http://localhost:8080/empdata/getAll')
+          .then(response => {
+            employee.value = response.data;
+          })
+          .catch(error => {
+            console.error(error);
+          });
+      }
+
+      // onMounted(()=>{
+      //   fetchEmployee();
+      // })
+  </script>
+  
+  <style>
+  /* Add your CSS styles here */
+  </style>
+  
